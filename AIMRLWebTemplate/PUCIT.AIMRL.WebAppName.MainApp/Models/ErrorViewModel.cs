b4e78854e790adfea49698a848c42c8f107ac05d
H.M.Zahid <hafizmzahid2@gmail.com>
2019-05-30 18:55:25 +0500

    Basic Project setup is complete.

---
+++ AIMRLWebTemplate/PUCIT.AIMRL.WebAppName.MainApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PUCIT.AIMRL.WebAppName.MainApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
